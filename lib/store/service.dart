834278ca039d54fde59022ebe7ce70ca3ba09807
ereio <github@ere.io>
2020-05-06 22:40:35 -0400

    background service is syncing and sending notifications when a user messages on android

--- lib/store/service.dart
+++ lib/store/service.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 import 'dart:isolate';
 import 'dart:math';
 import 'package:Tether/global/libs/hive/index.dart';
+import 'package:Tether/store/rooms/events/model.dart';
 import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 
@@ -34,8 +35,14 @@ class BackgroundSync {
     String accessToken,
     String lastSince,
   }) async {
-    print('[BackgroundSync] Starting Background Sync Service');
+    if (!Platform.isAndroid) {
+      print(
+        '[BackgroundSync] Failed initialization due to non-android platform :(',
+      );
+      return;
+    }
 
+    print('[BackgroundSync] Starting Background Sync Service');
     final backgroundServiceHive = await initHiveBackgroundServiceUnsafe();
     await backgroundServiceHive.put('accessToken', accessToken);
 
@@ -65,21 +72,6 @@ class BackgroundSync {
 }
 
 /**
- *  () {
-        try {
-          backgroundSync(
-            protocol: protocol,
-            homeserver: homeserver,
-            accessToken: accessToken,
-            lastSince: lastSince,
-          );
-        } catch (err) {
-          print(err);
-        }
-      },
- */
-
-/**
  * Background Sync Job (Android Only)
  * 
  * Fetches data from matrix in background and displays
@@ -100,20 +92,18 @@ void backgroundSyncJob() async {
 
     Box backgroundCache;
 
-    if (Platform.isIOS || Platform.isAndroid) {
-      // Init storage location
-      var storageLocation;
-      try {
-        storageLocation = await getApplicationDocumentsDirectory();
-      } catch (error) {
-        print('[initHiveStorage] storage location failure - $error');
-      }
-
-      // Init hive cache + adapters
-      Hive.init(storageLocation.path);
-      backgroundCache = await Hive.openBox(Cache.backgroundServiceBox);
-      accessToken = backgroundCache.get('accessToken');
+    // Init storage location
+    var storageLocation;
+    try {
+      storageLocation = await getApplicationDocumentsDirectory();
+    } catch (error) {
+      print('[initHiveStorage] storage location failure - $error');
     }
+
+    // Init hive cache + adapters
+    Hive.init(storageLocation.path);
+    backgroundCache = await Hive.openBox(Cache.backgroundServiceBox);
+    accessToken = backgroundCache.get('accessToken');
 
     // TODO: remove onSelect handler
     FlutterLocalNotificationsPlugin pluginInstance = await initNotifications();
@@ -127,46 +117,61 @@ void backgroundSyncJob() async {
     for (int i = 0; i < secondsTimeout; i++) {
       if (i % syncInterval == 0) {
         Timer(Duration(seconds: i), () async {
-          final DateTime now = DateTime.now();
-
-          // Check isolate id and maybe see if a new one is created
-          print(
-            "[AndroidAlarmService] Running Fetch Sync $i timestamp=$now isolate=$isolateId",
-          );
-
-          // TODO: fetchSync here and check for new messages,
-          // TODO: save an alternate lastSince for the background service only
-          final data = await fetchSyncIsolate(
-            protocol: protocol,
-            homeserver: homeserver,
-            accessToken: accessToken,
-            since: backgroundCache.get(SINCE_CACHE_KEY),
-          );
-
-          final String lastSince = data['next_batch'];
-          final Map<String, dynamic> rawRooms = data['rooms']['join'];
-
-          backgroundCache.put(SINCE_CACHE_KEY, lastSince);
-
-          print(
-            "[AndroidAlarmService] New Last Since ${data['next_batch']}",
-          );
-
-          print(
-            "[AndroidAlarmService] Room Data $rawRooms}",
-          );
-
-          /**
-         * Check last since and see if any new messages arrived in the payload
-         * No need to update the hive store for now, just do not save the lastSince
-         * to the store and the next foreground fetchSync will update the state
-         */
-
-          if (false) {
-            showMessageNotification(
-              messageHash: Random.secure().nextInt(20000),
-              pluginInstance: pluginInstance,
+          try {
+            // Check isolate id and maybe see if a new one is created
+            print(
+              "[AndroidAlarmService] Running Fetch Sync $i timestamp=${DateTime.now()} isolate=$isolateId",
             );
+
+            /**
+             * Check last since and see if any new messages arrived in the payload
+             * No need to update the hive store for now, just do not save the lastSince
+             * to the store and the next foreground fetchSync will update the state
+             */
+            final data = await fetchSyncIsolate(
+              protocol: protocol,
+              homeserver: homeserver,
+              accessToken: accessToken,
+              since: backgroundCache.get(SINCE_CACHE_KEY),
+            );
+
+            final String lastSince = data['next_batch'];
+            final Map<String, dynamic> rawRooms = data['rooms']['join'];
+
+            rawRooms.forEach((roomId, room) {
+              /**
+              *TODO: Need to handle group / bigger room chats differently than direct chats
+              */
+              final List<dynamic> timelineEvents = room['timeline']['events'];
+
+              if (timelineEvents != null && timelineEvents.length > 0) {
+                final messageEvent = timelineEvents.singleWhere(
+                  (element) => element['type'] == EventTypes.MESSAGE,
+                  orElse: () => null,
+                );
+
+                if (messageEvent != null) {
+                  final messageSender = messageEvent['sender'].split(':')[0];
+                  showMessageNotification(
+                    messageHash: Random.secure().nextInt(20000),
+                    body: '$messageSender sent you a new message.',
+                    pluginInstance: pluginInstance,
+                  );
+                }
+              }
+            });
+
+            backgroundCache.put(SINCE_CACHE_KEY, lastSince);
+
+            print(
+              "[AndroidAlarmService] New Last Since ${data['next_batch']}",
+            );
+
+            print(
+              "[AndroidAlarmService] Room Data $rawRooms}",
+            );
+          } catch (error) {
+            print('[BackgroundSync Service] Fetching Sync Failure $error');
           }
         });
       }
