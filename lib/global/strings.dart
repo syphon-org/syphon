b6b719ec7d18e957fb73ba2592500a4561719bc2
Ed Geraghty <ed@geraghty.london>
2021-10-22 19:45:46 +0100

    feat: start adding logic to the devices screen

--- lib/global/strings.dart
+++ lib/global/strings.dart
@@ -43,6 +43,7 @@ class Strings {
   static final titleDialogSignupEmailVerification =
       tr('title-dialog-email-requirement-verified');
   static final titleConfirmPassword = tr('title-confirm-password');
+  static final titleRenameDevice = tr('title-device-rename');
   static final titleConfirmDeleteKeys = tr('title-dialog-delete-keys');
   static final titleConfirmEmail = tr('title-confirm-email');
 
@@ -163,6 +164,7 @@ class Strings {
   static final contentPasswordRecommendation =
       tr('content-password-recommendation');
   static final contentDeleteDevices = tr('content-dialog-devices-delete');
+  static final contentRenameDevice = tr('content-dialog-device-rename');
   static final contentKeyExportWarning =
       tr('content-dialog-devices-key-export');
   static final contentEmailRequirement = tr('content-signup-email-requirement');
