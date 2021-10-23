14647e2aa8b8b1d19bc3378aa0b69457d9d91b70
Florian Sabonchi <sabonchi@posteo.de>
2021-10-23 22:20:38 +0200

    Delete Message Event updated

--- lib/global/libs/matrix/events.dart
+++ lib/global/libs/matrix/events.dart
@@ -34,6 +34,33 @@ abstract class Events {
     return await json.decode(response.body);
   }
 
+  /// Delete Message - PUT
+  /// https://matrix.org/docs/spec/client_server/r0.6.1#m-room-redaction
+  static Future<dynamic> deleteMessage({
+    String? protocol = 'https://',
+    String? homeserver = Values.homeserverDefault,
+    String? accessToken,
+    String? roomId,
+    String? eventId,
+    String? txnId,
+
+  })async{
+    final String url = '$protocol$homeserver/_matrix/client/r0/rooms/$roomId/redact/$eventId/$txnId';
+
+    final Map<String, String> headers = {
+      'Authorization': 'Bearer $accessToken',
+      ...Values.defaultHeaders,
+    };
+
+    final Map body = {};
+
+    final response = await http.put(Uri.parse(url), headers:  headers, body: json.encode(body));
+
+    return await json.decode(
+      response.body,
+    );
+  }
+
   /// Fetch Message Events
   /// https://matrix.org/docs/spec/client_server/latest#id261
   static Future<dynamic> fetchMessageEvents({
