6daff452ab7b5bf498533db796ba3a827214ce5c
Ed Geraghty <ed@geraghty.london>
2021-11-23 21:30:57 +0000

    feat: Add a proxy-aware client method

--- lib/global/https.dart
+++ lib/global/https.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
+import 'package:syphon/store/settings/http-proxy-settings/model.dart';
 
 /// This is LetsEncrypt's self-signed trusted root certificate authority
 /// certificate, issued under common name: ISRG Root X1 (Internet Security
@@ -72,6 +73,15 @@ HttpClient customHttpClient({String? cert}) {
 /// LetsEncrypt trusted certificate
 http.Client createClient() {
   return IOClient(customHttpClient(cert: ISRG_X1));
+}
+
+http.Client createProxiedClient(HttpProxySettings httpProxySettings) {
+  HttpClient customClient = customHttpClient(cert: ISRG_X1);
+  customClient.findProxy = (uri) {
+    return 'PROXY ${httpProxySettings.host}:${httpProxySettings.port};';
+  };
+
+  return IOClient(customClient);
 }
 
 // WARN: global var for httpClient requests
