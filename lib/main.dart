c16abd21f34cb560f2e1532444ac3649525a31f7
Ed Geraghty <ed@geraghty.london>
2021-11-23 21:31:26 +0000

    fix: call proxiedClient invocation if required

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 
-import 'package:http_proxy_override/http_proxy_override.dart';
-
 import 'package:syphon/cache/index.dart';
 import 'package:syphon/context/index.dart';
 import 'package:syphon/global/platform.dart';
@@ -11,6 +9,8 @@ import 'package:syphon/storage/index.dart';
 import 'package:syphon/store/index.dart';
 import 'package:syphon/views/prelock.dart';
 import 'package:syphon/views/syphon.dart';
+
+import 'global/https.dart';
 
 // ignore: avoid_void_async
 void main() async {
@@ -36,12 +36,7 @@ void main() async {
 
   // init http proxy, if required
   if (store.state.settingsStore.httpProxySettings.enabled) {
-    final HttpProxyOverride httpProxy = await HttpProxyOverride.createHttpProxy();
-
-    httpProxy.port = store.state.settingsStore.httpProxySettings.port;
-    httpProxy.host = store.state.settingsStore.httpProxySettings.host;
-
-    HttpOverrides.global = httpProxy;
+    httpClient = createProxiedClient(store.state.settingsStore.httpProxySettings);
   }
 
   // init app
