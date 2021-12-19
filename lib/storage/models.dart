db82a329d7fddf038e9dd4db11125a16b96281b2
ereio <git@ere.io>
2021-12-19 09:19:05 -0500

    fix: build issues with json gen - not needed

--- lib/storage/models.dart
+++ lib/storage/models.dart
@@ -1,11 +1,7 @@
 import 'dart:isolate';
 
-import 'package:json_annotation/json_annotation.dart';
 import 'package:syphon/global/values.dart';
 
-part 'models.g.dart';
-
-@JsonSerializable()
 class DatabaseInfo {
   final String key;
   final String path;
@@ -16,7 +12,4 @@ class DatabaseInfo {
     this.path = Values.empty,
     this.port,
   });
-
-  Map<String, dynamic> toJson() => _$DatabaseInfoToJson(this);
-  factory DatabaseInfo.fromJson(Map<String, dynamic> json) => _$DatabaseInfoFromJson(json);
 }
