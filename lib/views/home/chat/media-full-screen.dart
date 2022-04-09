629d3b21ad4c578a7df2711f502708f9a01ef981
Florian Sabonchi <sabonchi@posteo.de>
2022-04-09 02:44:53 +0200

    some adjustments

--- lib/views/home/chat/media-full-screen.dart
+++ lib/views/home/chat/media-full-screen.dart
@@ -1,18 +1,26 @@
+import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:downloads_path_provider/downloads_path_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'package:photo_view/photo_view.dart';
-import 'package:syphon/store/settings/theme-settings/model.dart';
+import 'package:share_plus/share_plus.dart';
+
 
 class MediaFullScreen extends StatelessWidget {
   final String title;
   final Uint8List bytes;
+  final String? roomId;
+  final String? eventId;
 
   const MediaFullScreen({
     Key? key,
+    required this.roomId,
     required this.title,
     required this.bytes,
+    required this.eventId,
   }) : super(key: key);
 
   @override
@@ -20,33 +28,43 @@ class MediaFullScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: () => {
-
-            }, icon: Icon(Icons.download)),
-          IconButton(onPressed: () => {
-
-            }, icon: Icon(Icons.share)),
-          IconButton(onPressed: () => {
-
-            }, icon: Icon(Icons.open_in_browser)),
-        ],
-        title: Column(children: [
-
-          Text('View embed', style: TextStyle(fontSize: 18,
-              fontWeight: FontWeight.bold),
-          ),
-          Text(title, style: TextStyle(
+          IconButton(icon: Icon(Icons.download),
               color: Colors.white,
-              fontSize: 10
-          )),
-        ]),
+              onPressed:  () async {
+               
+              }
+          ),
+          IconButton(icon: Icon(Icons.share),
+              color: Colors.white,
+              onPressed: () async {
+                await Share.share('https://matrix.to/#/$roomId/$eventId');
+              }
+          ),
+        ],
+        title: Column(
+            children: [
+              Text(
+                  'View embed',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                  )
+              ),
+              Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,)
+              ),
+            ]
+        ),
         leading: IconButton(
           onPressed: () => {
             Navigator.of(context).pop(),
           },
-          icon: Icon(
-            Icons.arrow_back_outlined,
-          ),
+          icon: Icon(Icons.arrow_back_outlined,),
+          color: Colors.white,
         ),
       ),
       body: PhotoView(
