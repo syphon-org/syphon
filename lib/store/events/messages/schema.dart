39a1bbf6d7a447a422fddd92682d2062456e8d80
Ed Geraghty <ed@geraghty.london>
2022-04-11 00:08:18 +0100

    fix: add missing column

--- lib/store/events/messages/schema.dart
+++ lib/store/events/messages/schema.dart
@@ -94,6 +94,7 @@ class Decrypted extends Table {
   // Message timestamps
   IntColumn get timestamp => integer()();
   IntColumn get received => integer()();
+  IntColumn get selfDestructAfter => integer()();
 
   // Message Only
   TextColumn get body => text().nullable()();
