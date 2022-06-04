518618f47392bbf3205cde84d0b62d4d93d05da8
Florian Sabonchi <sabonchi@posteo.de>
2022-06-05 00:12:56 +0200

    first draft for copy username by holding

--- lib/views/widgets/modals/modal-user-details.dart
+++ lib/views/widgets/modals/modal-user-details.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:redux/redux.dart';
@@ -8,6 +9,7 @@ import 'package:syphon/global/assets.dart';
 import 'package:syphon/global/colors.dart';
 import 'package:syphon/global/dimensions.dart';
 import 'package:syphon/global/strings.dart';
+import 'package:syphon/store/alerts/actions.dart';
 import 'package:syphon/store/index.dart';
 import 'package:syphon/store/rooms/actions.dart';
 import 'package:syphon/store/rooms/selectors.dart';
@@ -19,6 +21,8 @@ import 'package:syphon/views/home/search/search-chats-screen.dart';
 import 'package:syphon/views/navigation.dart';
 import 'package:syphon/views/widgets/avatars/avatar.dart';
 import 'package:syphon/views/widgets/dialogs/dialog-start-chat.dart';
+
+import '../../syphon.dart';
 
 class ModalUserDetails extends StatelessWidget {
   const ModalUserDetails({
@@ -170,12 +174,15 @@ class ModalUserDetails extends StatelessWidget {
                       children: <Widget>[
                         Flexible(
                           // wrapped with flexible to allow ellipsis
-                          child: Text(
+                          child: InkWell(child: Text(
                             props.user.userId ?? '',
                             overflow: TextOverflow.ellipsis,
                             textAlign: TextAlign.center,
                             style: Theme.of(context).textTheme.subtitle1,
-                          ),
+                          ), onTap: () async {
+                              await Clipboard.setData(ClipboardData(text: props.user.userId));
+                              await props.onAddConfirmation('Username copied to clipboard');
+                          }),
                         ),
                       ],
                     ),
@@ -276,8 +283,10 @@ class _Props extends Equatable {
   final Map<String, User> users;
   final Function onBlockUser;
   final Function onCreateChatDirect;
+  final Function onAddConfirmation;
 
   const _Props({
+    required this.onAddConfirmation,
     required this.user,
     required this.users,
     required this.loading,
@@ -331,5 +340,8 @@ class _Props extends Equatable {
             invites: <User>[user],
           ),
         ),
+        onAddConfirmation: ({required String message}) async {
+          await store.dispatch(addConfirmation(message: message,));
+        }
       );
 }
